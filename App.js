13c79627e2a92a11d54f0e65288c97c867aa6bde
Shavi Sharma <134057557+shavi1167@users.noreply.github.com>
2025-06-23 17:44:41 +0530

    Add files via upload
    The React Stopwatch App is a lightweight, interactive timer application built using React.js. It offers users the ability to measure elapsed time with accuracy down to milliseconds. The interface features intuitive Start, Stop, and Reset buttons, updating the time in real-time without page reloads.
    
    This project is a great learning tool for beginners exploring React functional components, React Hooks (useState, useEffect), and how to manage time-based updates using JavaScript’s setInterval() method.

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+import Stopwatch from './component/stopwatch'
+
+function App() {
+  return (
+    <>
+      <Stopwatch />
+    </>
+  );
+}
+
+export default App;
